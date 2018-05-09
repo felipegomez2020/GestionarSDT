7a1ba548c270493cdc93dfdfbec19cc847113523
trilleras <felipegomez202019@gmail.com>
2018-05-09 18:51:17 -0500

    ojo

--- gestionar/Aplicacion/models.py
+++ gestionar/Aplicacion/models.py
@@ -60,7 +60,7 @@ class Afiliado (models.Model):
         verbose_name_plural = 'Afiliados'
     
     
-    "hol"
+    "ho"
 class Benefiniciario(models.Model):
     nombres = models.CharField(max_length=30)
     apellidos = models.CharField(max_length=30)
