b67a0df6b299d20773b4855860d4ba1d667e16eb
Development <developer@gmail.com>
2018-05-09 12:04:30 -0500

    cambios no interrumpen nada

--- gestionar/gestionar/settings.py
+++ gestionar/gestionar/settings.py
@@ -83,10 +83,6 @@ AUTHENTICATION_BACKENDS = (
 
 WSGI_APPLICATION = 'gestionar.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
