7075e13c33b9b2a84e0fffb7906c987055589306
felipegomez2020 <37225024+felipegomez2020@users.noreply.github.com>
2018-05-29 00:09:35 -0500

    Bug corregido

--- gestionar/gestionar/urls.py
+++ gestionar/gestionar/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import url, include
-fron Aplicacion.admin import admin_site
+from Aplicacion.admin import admin_site
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
