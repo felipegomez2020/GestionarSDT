2b38804b882a6649460d894841973a91211efb15
felipegomez2020 <37225024+felipegomez2020@users.noreply.github.com>
2018-05-28 23:55:00 -0500

    admin.site modificado

--- gestionar/gestionar/urls.py
+++ gestionar/gestionar/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import url, include
+fron django.Aplicacion.admin import admin_site
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rest/', include('Aplicacion.ApiRest.urls')),
+    url(r'^myadmin/', admin_site.urls),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
