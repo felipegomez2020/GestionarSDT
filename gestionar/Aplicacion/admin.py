54cd617c898cc501ea73315ff25a5d8db5d21db9
felipegomez2020 <37225024+felipegomez2020@users.noreply.github.com>
2018-05-28 23:49:44 -0500

    URL ver sitio

--- gestionar/Aplicacion/admin.py
+++ gestionar/Aplicacion/admin.py
@@ -2,8 +2,15 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 from Aplicacion.models import UsuarioAdministrativo, Afiliado, Benefiniciario,\
     Ingreso,CitaMedica
+    
+class MyAdminSite(AdminSite):
+    site_url = 'http://52.91.174.233:8180/admin/'
+
+admin_site = MyAdminSite(name='myadmin')
+
     
     
 admin.site.site_header = "Gestionar"
