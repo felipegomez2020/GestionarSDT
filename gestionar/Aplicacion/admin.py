414f4760867b4ee08fed72ccd0219d008acda3c4
felipegomez2020 <37225024+felipegomez2020@users.noreply.github.com>
2018-05-29 00:36:19 -0500

    Update admin.py

--- gestionar/Aplicacion/admin.py
+++ gestionar/Aplicacion/admin.py
@@ -6,16 +6,16 @@ from django.contrib.admin import AdminSite
 from Aplicacion.models import UsuarioAdministrativo, Afiliado, Benefiniciario,\
     Ingreso,CitaMedica
     
-class MyAdminSite(AdminSite):
-    site_url = 'http://52.91.174.233:8180/admin/'
+class MyModelAdmin(admin.ModelAdmin):
+    view_on_site = False
 
-admin_site = MyAdminSite(name='myadmin')
 
     
     
 admin.site.site_header = "Gestionar"
 admin.site.site_title = "Gestionar"
 
+admin.site.register(MyModel,MyModelAdmin)
 admin.site.register(UsuarioAdministrativo)
 admin.site.register(Afiliado)
 admin.site.register(Benefiniciario)
